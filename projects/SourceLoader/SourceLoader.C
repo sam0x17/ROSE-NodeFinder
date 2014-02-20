29cde6aae120b5e41419df78a4196feb88b76fba
SamKelly <sam@durosoft.com>
2014-02-20 03:02:40 -0500

    (SourceLoader) Adds project skeleton for SourceLoader

---
+++ projects/SourceLoader/SourceLoader.C
@@ -0,0 +1,9 @@
+/*
+ * SourceLoader.C
+ *
+ *  Created on: Feb 5, 2014
+ *      Author: Sam Kelly <kellys@dickinson.edu>
+ */
+#include <SourceLoader.h>
+
+
---
+++ projects/SourceLoader/SourceLoader.h
@@ -0,0 +1,11 @@
+/*
+ * SourceLoader.h
+ *
+ *  Created on: Feb 5, 2014
+ *      Author: Sam Kelly <kellys@dickinson.edu>
+ */
+#ifndef ROSE_Project_SourceLoader_H
+#define ROSE_Project_SourceLoader_H
+
+
+#endif
